c839906d1e56ad20e91aaafe9fca8deee664f682
stevenhorsman <steven@uk.ibm.com>
2019-02-19 14:43:03 +0000

    Make singular
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

--- data-sets-model/src/main/java/org/zowe/data/sets/model/AllocationUnitType.java
+++ data-sets-model/src/main/java/org/zowe/data/sets/model/AllocationUnitType.java
@@ -13,5 +13,5 @@ import io.swagger.annotations.ApiModel;
 
 @ApiModel
 public enum AllocationUnitType {
-    TRACK, CYLINDER, BLOCK, BYTE, KILOBYTES, MEGABYTES, BYTES, RECORDS;
+    TRACK, CYLINDER, BLOCK, BYTE, KILOBYTE, MEGABYTE, RECORD;
 }
