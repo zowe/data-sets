84bea658076c6888684bf3d50dfff34ea44c604e
stevenhorsman <steven@uk.ibm.com>
2019-02-25 15:45:45 +0000

    Add custom exception handling for locked data set
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- data-sets-api-server/src/main/java/org/zowe/data/sets/services/zosmf/AbstractZosmfDataSetsRequestRunner.java
+++ data-sets-api-server/src/main/java/org/zowe/data/sets/services/zosmf/AbstractZosmfDataSetsRequestRunner.java
@@ -16,6 +16,7 @@ import org.apache.http.HttpStatus;
 import org.zowe.api.common.connectors.zosmf.exceptions.DataSetNotFoundException;
 import org.zowe.api.common.exceptions.ZoweApiRestException;
 import org.zowe.api.common.zosmf.services.AbstractZosmfRequestRunner;
+import org.zowe.data.sets.exceptions.DataSetLockedException;
 import org.zowe.data.sets.exceptions.UnauthorisedDataSetException;
 
 public abstract class AbstractZosmfDataSetsRequestRunner<T> extends AbstractZosmfRequestRunner<T> {
@@ -28,6 +29,11 @@ public abstract class AbstractZosmfDataSetsRequestRunner<T> extends AbstractZosm
         if (statusCode == HttpStatus.SC_INTERNAL_SERVER_ERROR) {
             if (details.toString().contains(AUTHORIZATION_FAILURE)) {
                 throw new UnauthorisedDataSetException(dataSetName);
+            } else if (details.toString().contains("IEFA110I")) {
+                // Extract the last line which has the details
+                String[] commaSplit = details.toString().split(",");
+                String[] dataLine = commaSplit[commaSplit.length - 1].replace("\"", "").split("\\s+");
+                throw new DataSetLockedException(dataSetName, dataLine[0], dataLine[1], dataLine[2]);
             }
         } else if (statusCode == HttpStatus.SC_NOT_FOUND) {
             if (details.toString().contains(DATA_SET_NOT_FOUND)) {
