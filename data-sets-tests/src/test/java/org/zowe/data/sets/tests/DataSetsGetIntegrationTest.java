833361c28f743a51afc6985b9b3f643d1bcebbe0
temanbrcom <andrei.temnikov@broadcom.com>
2019-03-12 10:22:59 +0100

    Refactored integration tests.
    Signed-off-by: temanbrcom <andrei.temnikov@broadcom.com>

--- data-sets-tests/src/test/java/org/zowe/data/sets/tests/DataSetsGetIntegrationTest.java
+++ data-sets-tests/src/test/java/org/zowe/data/sets/tests/DataSetsGetIntegrationTest.java
@@ -14,8 +14,11 @@ import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.zowe.data.sets.model.DataSet;
+import org.zowe.data.sets.model.DataSetAttributes;
 import org.zowe.data.sets.model.DataSetCreateRequest;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.hasItem;
@@ -23,34 +26,72 @@ import static org.junit.Assert.assertThat;
 
 public class DataSetsGetIntegrationTest extends AbstractDataSetsIntegrationTest {
 
+    public static final String TEMP_DATA_SET = HLQ + ".TEST.DELETE";
+
     @Test
-    public void testGetValidDataset() throws Exception {
-        String tempDataSet = HLQ + ".TEST.DELETE";
-        DataSetCreateRequest pdsRequest = createPdsRequest(tempDataSet);
+    public void testGetValidDataSetAttributes() throws Exception {
+        DataSetCreateRequest pdsRequest = createPdsRequest(TEMP_DATA_SET);
         createDataSet(pdsRequest);
 
         try {
-            DataSet expected = DataSet.builder().name(tempDataSet).migrated(false).build();
 
-            List<DataSet> actual = getDataSets(tempDataSet).then().statusCode(HttpStatus.SC_OK).extract()
+            String pattern = "yyyy/MM/dd";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+            String today = simpleDateFormat.format(new Date());
+
+            DataSetAttributes expected = DataSetAttributes.builder().blockSize(pdsRequest.getBlockSize())
+                    .deviceType("3390").creationDate(today).name(TEMP_DATA_SET).migrated(false)
+                    .dataSetOrganization(pdsRequest.getDataSetOrganization()).expirationDate("***None***")
+                    .recordLength(pdsRequest.getRecordLength()).allocationUnit(pdsRequest.getAllocationUnit())
+                    .recordFormat(pdsRequest.getRecordFormat()).allocatedSize(10).used(10).build();
+
+            List<DataSetAttributes> actual = getDataSetsDetails(TEMP_DATA_SET).then().statusCode(HttpStatus.SC_OK).extract()
+                    .body().jsonPath().getList("items", DataSetAttributes.class);
+            // We can't tell the value of some attributes
+            for (DataSetAttributes dataSetAttributes : actual) {
+                dataSetAttributes.setCatalogName(null);
+                dataSetAttributes.setVolumeSerial(null);
+            }
+
+            assertThat(actual, hasItem(expected));
+        } finally {
+            deleteDataSet(TEMP_DATA_SET);
+        }
+    }
+
+    @Test
+    public void testGetValidDataSet() throws Exception {
+        DataSetCreateRequest pdsRequest = createPdsRequest(TEMP_DATA_SET);
+        createDataSet(pdsRequest);
+
+        try {
+            DataSet expected = DataSet.builder().name(TEMP_DATA_SET).migrated(false).build();
+
+            List<DataSet> actual = getDataSets(TEMP_DATA_SET).then().statusCode(HttpStatus.SC_OK).extract()
                     .body().jsonPath().getList("items", DataSet.class);
             assertThat(actual, hasItem(expected));
         } finally {
-            deleteDataSet(tempDataSet);
+            deleteDataSet(TEMP_DATA_SET);
         }
     }
 
     // TODO - add better test of multiple data sets?
 
     @Test
-    public void testGetInvalidDatasets() throws Exception {
+    public void testGetInvalidDataSets() throws Exception {
         getDataSets(INVALID_DATASET_NAME).then().statusCode(HttpStatus.SC_OK).body("items", IsEmptyCollection.empty());
+    }
+
+    @Test
+    public void testGetInvalidDataSetAttributes() throws Exception {
+        getDataSetsDetails(INVALID_DATASET_NAME).then().statusCode(HttpStatus.SC_OK).body("items", IsEmptyCollection.empty());
     }
 
     @Test
     // TODO - need to create the unauthorised dataset in setup script
     @Ignore("Task 19604")
-    public void testGetUnauthorisedDatasetMembers() throws Exception {
+    public void testGetUnauthorisedDataSetMembers() throws Exception {
         getDataSets(UNAUTHORIZED_DATASET).then().statusCode(HttpStatus.SC_FORBIDDEN);
     }
 }
