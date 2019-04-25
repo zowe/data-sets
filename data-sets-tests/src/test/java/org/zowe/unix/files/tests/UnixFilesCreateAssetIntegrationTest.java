1a731d4326628d5197753e6262958f38957f7aa1
Jordan Cain <jordan.cain1@uk.ibm.com>
2019-04-25 14:44:43 -0400

    Fix integration test style errors
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

--- data-sets-tests/src/test/java/org/zowe/unix/files/tests/UnixFilesCreateAssetIntegrationTest.java
+++ data-sets-tests/src/test/java/org/zowe/unix/files/tests/UnixFilesCreateAssetIntegrationTest.java
@@ -1,23 +1,31 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2019
+ */
 package org.zowe.unix.files.tests;
-
-import static org.junit.Assert.assertEquals;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.apache.http.HttpStatus;
-import org.junit.Test;
-import org.springframework.http.MediaType;
-import org.zowe.api.common.errors.ApiError;
-import org.zowe.unix.files.exceptions.InvalidPermissionsException;
-import org.zowe.unix.files.exceptions.AlreadyExistsException;
-import org.zowe.unix.files.model.UnixEntityType;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.zowe.api.common.errors.ApiError;
+import org.zowe.unix.files.exceptions.AlreadyExistsException;
+import org.zowe.unix.files.exceptions.InvalidPermissionsException;
+import org.zowe.unix.files.model.UnixEntityType;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 public class UnixFilesCreateAssetIntegrationTest extends AbstractUnixFilesIntegrationTest {
     
