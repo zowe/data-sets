523e86470507ac283e2270024e6cb2f1b70d11ed
Craig-Forrest <Craig.Forrest@ibm.com>
2019-05-20 09:39:08 +0100

    add logout tests for /unixfiles and /datasets
    Signed-off-by: Craig-Forrest <Craig.Forrest@ibm.com>

--- data-sets-tests/src/test/java/org/zowe/tests/AbstractFilesIntegrationTest.java
+++ data-sets-tests/src/test/java/org/zowe/tests/AbstractFilesIntegrationTest.java
@@ -9,8 +9,24 @@
  */
 package org.zowe.tests;
 
+import io.restassured.RestAssured;
+
+import org.apache.http.HttpStatus;
 import org.zowe.api.common.test.AbstractHttpIntegrationTest;
 
 public abstract class AbstractFilesIntegrationTest extends AbstractHttpIntegrationTest {
     protected static final String HEX_IN_QUOTES_REGEX = "^\"[0-9A-F]+\"$";
+
+    public void testLogout(String path) {
+        String cookie = RestAssured.given().when().get(path).getCookie("JSESSIONID");
+
+        RestAssured.given().when().auth().none().cookie("JSESSIONID", cookie).get(path)
+            .then().statusCode(HttpStatus.SC_OK);
+
+        RestAssured.given().when().auth().none().cookie("JSESSIONID", cookie).get("/logout")
+            .then().statusCode(HttpStatus.SC_NO_CONTENT);
+
+        RestAssured.given().when().auth().none().cookie("JSESSIONID", cookie).get(path)
+        .then().statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
 }
