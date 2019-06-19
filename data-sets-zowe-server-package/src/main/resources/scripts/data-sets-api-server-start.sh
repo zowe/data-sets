b7aca7400e9506118fb93c5f7503311d2aaa5edc
stevenhorsman <steven@uk.ibm.com>
2019-06-19 11:42:29 +0100

    Update properties used in start script
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- data-sets-zowe-server-package/src/main/resources/scripts/data-sets-api-server-start.sh
+++ data-sets-zowe-server-package/src/main/resources/scripts/data-sets-api-server-start.sh
@@ -1,13 +1,13 @@
 # Variables to be replaced:
-# - JAVA_SETUP -- sets JAVA_HOME by ZOWE_JAVA_HOME
-# - SERVER_PORT - The port the jobs server will use
-# - SERVER_PORT - The port the data sets server will use
+# - ZOWE_JAVA_HOME - JAVA_HOME that zowe is using
+# - FILES_API_PORT - The port the data sets server will use
 # - KEYSTORE - The keystore to use for SSL certificates
 # - KEYSTORE_PASSWORD - The password to access the keystore supplied by KEYSTORE
 # - KEY_ALIAS - The alias of the key within the keystore
-# - ZOSMF_HTTPS_PORT - The SSL port z/OSMF is listening on.
-# - ZOSMF_IP - The IP Address z/OSMF can be reached
-**JAVA_SETUP**
+# - ZOSMF_PORT - The SSL port z/OSMF is listening on.
+# - ZOSMF_IP_ADDRESS - The IP Address z/OSMF can be reached
+
+export JAVA_HOME=$ZOWE_JAVA_HOME
 if [[ ":$PATH:" == *":$JAVA_HOME/bin:"* ]]; then
   echo "ZOWE_JAVA_HOME already exists on the PATH"
 else
@@ -15,14 +15,16 @@ else
   export PATH=$PATH:$JAVA_HOME/bin
   echo "Done."
 fi
+
 DIR=`dirname $0`
+
 java -Xms16m -Xmx512m -Dibm.serversocket.recover=true -Dfile.encoding=UTF-8 \
     -Djava.io.tmpdir=/tmp -Xquickstart \
-    -Dserver.port=**SERVER_PORT** \
-    -Dserver.ssl.keyAlias=**KEY_ALIAS** \
-    -Dserver.ssl.keyStore=**KEYSTORE** \
-    -Dserver.ssl.keyStorePassword=**KEYSTORE_PASSWORD** \
+    -Dserver.port=${FILES_API_PORT} \
+    -Dserver.ssl.keyAlias=${KEY_ALIAS} \
+    -Dserver.ssl.keyStore=${KEYSTORE} \
+    -Dserver.ssl.keyStorePassword=${KEYSTORE_PASSWORD} \
     -Dserver.ssl.keyStoreType=PKCS12 \
-    -Dzosmf.httpsPort=**ZOSMF_HTTPS_PORT** \
-    -Dzosmf.ipAddress=**ZOSMF_IP** \
-    -jar $DIR/../**DATASETS_JAR** &
+    -Dzosmf.httpsPort=${ZOSMF_PORT} \
+    -Dzosmf.ipAddress=${ZOSMF_IP_ADDRESS} \
+    -jar $DIR/../{{jar_name}} &
