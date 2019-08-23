6d25225526ead4e5c500b85f5ea73227a12ffcc0
stevenhorsman <steven@uk.ibm.com>
2019-08-23 13:34:58 +0100

    Update error messaging
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- data-sets-zowe-server-package/src/main/resources/scripts/files-api-validate.sh
+++ data-sets-zowe-server-package/src/main/resources/scripts/files-api-validate.sh
@@ -8,23 +8,30 @@
 # Copyright IBM Corporation 2019
 ################################################################################
 
+FILES_API_ERRORS_FOUND=0
+
+error() {
+  echo "Error: $1"
+  let "FILES_API_ERRORS_FOUND=$FILES_API_ERRORS_FOUND+1"
+}
+
 # Configure and start require:
 # - ZOWE_PREFIX - should be <=5 char long and exist
 #TODO - any lower bound (other than 0)?
 PREFIX_LENGTH=${#ZOWE_PREFIX}
 if [[ -z $ZOWE_PREFIX ]]
 then
-  echo "Error: ZOWE_PREFIX is not set"
+  error "ZOWE_PREFIX is not set"
 elif [[ $PREFIX_LENGTH > 5 ]]
 then
-  echo "Error: ZOWE_PREFIX '$ZOWE_PREFIX' should be less than 6 characters"
+  error "ZOWE_PREFIX '$ZOWE_PREFIX' should be less than 6 characters"
 fi
 
 # - FILES_API_PORT - should not be bound to a port currently
 MATCHES=`onetstat -a | grep -c $FILES_API_PORT`
 if [[ $MATCHES > 0 ]]
 then
-    echo "Error: Port $FILES_API_PORT is already in use by process `netstat -a -P $FILES_API_PORT | grep Listen`"
+    error "Port $FILES_API_PORT is already in use by process `netstat -a -P $FILES_API_PORT | grep Listen`"
 fi
 
 # Mediation stuff, should validate in a separate script
@@ -33,15 +40,15 @@ fi
 # - KEY_ALIAS - The alias of the key within the keystore
 if [[ -z "${KEYSTORE}" ]]
 then 
-    echo "Error: KEYSTORE is empty"
+    error "KEYSTORE is empty"
 fi
 if [[ -z "${KEYSTORE_PASSWORD}" ]]
 then 
-    echo "Error: KEYSTORE_PASSWORD is empty"
+    error "KEYSTORE_PASSWORD is empty"
 fi
 if [[ -z "${KEY_ALIAS}" ]]
 then 
-    echo "Error: KEY_ALIAS is empty"
+    error "KEY_ALIAS is empty"
 fi
 
 # - ZOSMF_PORT - The SSL port z/OSMF is listening on.
@@ -64,31 +71,36 @@ const options = {
 
 const req = https.request(options, (res) => {
   console.log(res.statusCode);
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
 });
 req.end();
 EOF
 
-RESPONSE_CODE=`node httpRequest.js ${ZOSMF_IP_ADDRESS} ${ZOSMF_PORT}`
-if [[ -z "${RESPONSE_CODE}" ]]
-then
-  echo "Warning: Could not validate if z/OS MF is available on 'https://${ZOSMF_IP_ADDRESS}:${ZOSMF_PORT}/zosmf/info'"
+if [[ -z "${ZOSMF_IP_ADDRESS}" || -z "${ZOSMF_PORT}" ]]
+then 
+    error "ZOSMF_IP_ADDRESS and ZOSMF_PORT are not both set"
 else
-  if [[ $RESPONSE_CODE != 200 ]]
+  RESPONSE_CODE=`node httpRequest.js ${ZOSMF_IP_ADDRESS} ${ZOSMF_PORT}`
+  if [[ -z "${RESPONSE_CODE}" ]]
   then
-    echo "Error: Could not contact z/OS MF on 'https://${ZOSMF_IP_ADDRESS}:${ZOSMF_PORT}/zosmf/info'"
+    echo "Warning: Could not validate if z/OS MF is available on 'https://${ZOSMF_IP_ADDRESS}:${ZOSMF_PORT}/zosmf/info'"
+  else
+    if [[ $RESPONSE_CODE != 200 ]]
+    then
+      error "Could not contact z/OS MF on 'https://${ZOSMF_IP_ADDRESS}:${ZOSMF_PORT}/zosmf/info' - $RESPONSE_CODE"
+    fi
   fi
 fi
 
 # - STATIC_DEF_CONFIG_DIR - Should exist and be writable
-STATIC_DEF_CONFIG_DIR=/u/stevenh/435/runtime/api-mediation/api-defs/
-
 if [[ ! -d ${STATIC_DEF_CONFIG_DIR} ]]
 then	
-  echo "Error: Static definition config directory '${STATIC_DEF_CONFIG_DIR}' doesn't exist"
+  error "Static definition config directory '${STATIC_DEF_CONFIG_DIR}' doesn't exist"
 else 	
 	if [[ ! -w ${STATIC_DEF_CONFIG_DIR} ]]
 	then	
-	  echo "Error: Static definition config directory '${STATIC_DEF_CONFIG_DIR}' does not have write access"	
+	  error "Static definition config directory '${STATIC_DEF_CONFIG_DIR}' does not have write access"	
 	fi
 fi
 
@@ -100,10 +112,10 @@ then
     oping ${ZOWE_EXPLORER_HOST} > /dev/null    # check host
     if [[ $? -ne 0 ]]
     then    
-        echo "Error: ZOWE_EXPLORER_HOST '$ZOWE_EXPLORER_HOST' does not point to a valid hostname"
+        error "ZOWE_EXPLORER_HOST '$ZOWE_EXPLORER_HOST' does not point to a valid hostname"
     fi
 else 
-    echo "Error: ZOWE_EXPLORER_HOST is empty"
+    error "ZOWE_EXPLORER_HOST is empty"
 fi
 
 # TODO move to a all zowe setup/validate?
@@ -114,15 +126,15 @@ then
     if [[ $? -ne 0 ]]
     then
         echo Error: ZOWE_JAVA_HOME does not point to a valid install of Java
+    else
+      JAVA_VERSION=`${ZOWE_JAVA_HOME}/bin/java -version 2>&1 | grep ^"java version"`
+      if [[ "$JAVA_VERSION" < "java version \"1.8" ]]
+      then 
+        echo Error: $JAVA_VERSION is less than minimum level required of 1.8
+      fi
     fi
 else 
     echo Error: ZOWE_JAVA_HOME is empty
 fi
 
-JAVA_VERSION=`${ZOWE_JAVA_HOME}/bin/java -version 2>&1 | grep ^"java version"`
-if [[ "$JAVA_VERSION" < "java version \"1.8" ]]
-then 
-  echo Error: $JAVA_VERSION is less than minimum level required of 1.8
-fi
-
-#TODO - non-zero exit code on error?
+return $FILES_API_ERRORS_FOUND
