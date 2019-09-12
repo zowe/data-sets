e1e55d0008b5fd4a872a3e7a6e80bb5038d92cd9
stevenhorsman <steven@uk.ibm.com>
2019-09-12 10:59:20 +0100

    Centralise validation
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- data-sets-zowe-server-package/src/main/resources/scripts/files-api-validate.sh
+++ data-sets-zowe-server-package/src/main/resources/scripts/files-api-validate.sh
@@ -10,107 +10,23 @@
 # Copyright IBM Corporation 2019
 ################################################################################
 
-FILES_API_ERRORS_FOUND=0
+INITIAL_ERRORS_FOUND=$ERRORS_FOUND
 
 error() {
-  echo "Error $FILES_API_ERRORS_FOUND: $1"
-  let "FILES_API_ERRORS_FOUND=$FILES_API_ERRORS_FOUND+1"
+  . ${ROOT_DIR}/scripts/utils/error.sh $1
 }
 
-# Configure and start require:
-# - ZOWE_PREFIX - should be <=5 char long and exist
-#TODO - any lower bound (other than 0)?
-PREFIX_LENGTH=${#ZOWE_PREFIX}
-if [[ -z $ZOWE_PREFIX ]]
-then
-  error "ZOWE_PREFIX is not set"
-elif [[ $PREFIX_LENGTH > 5 ]]
-then
-  error "ZOWE_PREFIX '$ZOWE_PREFIX' should be less than 6 characters"
-fi
+. ${ROOT_DIR}/scripts/utils/validateZowePrefix.sh 
 
 # - FILES_API_PORT - should not be bound to a port currently
-MATCHES=`onetstat -a | grep -c $FILES_API_PORT`
-if [[ $MATCHES > 0 ]]
-then
-    error "Port $FILES_API_PORT is already in use by process `netstat -a -P $FILES_API_PORT | grep Listen`"
-fi
+. ${ROOT_DIR}/scripts/utils/validatePortAvailable.sh $FILES_API_PORT
 
 # Mediation stuff, should validate in a separate script
-# - KEYSTORE - The keystore to use for SSL certificates
-# - KEYSTORE_PASSWORD - The password to access the keystore supplied by KEYSTORE
-# - KEY_ALIAS - The alias of the key within the keystore
-if [[ -z "${KEYSTORE}" ]]
-then 
-    error "KEYSTORE is empty"
-fi
-if [[ -z "${KEYSTORE_PASSWORD}" ]]
-then 
-    error "KEYSTORE_PASSWORD is empty"
-fi
-if [[ -z "${KEY_ALIAS}" ]]
-then 
-    error "KEY_ALIAS is empty"
-fi
+. ${ROOT_DIR}/scripts/utils/validateApiMLVariables.sh 
 
 # - ZOSMF_PORT - The SSL port z/OSMF is listening on.
 # - ZOSMF_IP_ADDRESS - The IP Address z/OSMF can be reached
-# TODO - improve by creating in zowe-install-packaging in some sort of shared utils?
-cat <<EOF >httpRequest.js
-const https = require('https');
-
-const args = process.argv.slice(2)
-const zosmfIp=args[0];
-const zosmfPort=args[1];
-
-const options = {
-  hostname: zosmfIp,
-  port: zosmfPort,
-  path: '/zosmf/info',
-  method: 'GET',
-  rejectUnauthorized: false
-};
-
-const req = https.request(options, (res) => {
-  console.log(res.statusCode);
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-req.end();
-EOF
-
-if [[ -z "${ZOSMF_IP_ADDRESS}" || -z "${ZOSMF_PORT}" ]]
-then 
-    error "ZOSMF_IP_ADDRESS and ZOSMF_PORT are not both set"
-else
-  if [ ! -z "$NODE_HOME" ];
-  then
-    NODE_BIN=${NODE_HOME}/bin/node
-    RESPONSE_CODE=`$NODE_BIN httpRequest.js ${ZOSMF_IP_ADDRESS} ${ZOSMF_PORT}`
-    if [[ -z "${RESPONSE_CODE}" ]]
-    then
-      echo "Warning: Could not validate if z/OS MF is available on 'https://${ZOSMF_IP_ADDRESS}:${ZOSMF_PORT}/zosmf/info'"
-    else
-      if [[ $RESPONSE_CODE != 200 ]]
-      then
-        error "Could not contact z/OS MF on 'https://${ZOSMF_IP_ADDRESS}:${ZOSMF_PORT}/zosmf/info' - $RESPONSE_CODE"
-      fi
-    fi
-  else
-    echo "Warning: Could not validate if z/OS MF is available on 'https://${ZOSMF_IP_ADDRESS}:${ZOSMF_PORT}/zosmf/info'"
-  fi
-fi
-
-# - STATIC_DEF_CONFIG_DIR - Should exist and be writable
-if [[ ! -d ${STATIC_DEF_CONFIG_DIR} ]]
-then	
-  error "Static definition config directory '${STATIC_DEF_CONFIG_DIR}' doesn't exist"
-else 	
-	if [[ ! -w ${STATIC_DEF_CONFIG_DIR} ]]
-	then	
-	  error "Static definition config directory '${STATIC_DEF_CONFIG_DIR}' does not have write access"	
-	fi
-fi
+. ${ROOT_DIR}/scripts/utils/validateZosmfHostAndPort.sh
 
 # Not sure how we validate - just exist ok? dig/oping?
 #TODO - use oping, or the switcher in zowe-install-packaging utils?
@@ -126,23 +42,6 @@ else
     error "ZOWE_EXPLORER_HOST is empty"
 fi
 
-# TODO move to a all zowe setup/validate?
-# ZOWE_JAVA_HOME Should exist, be version 8+ and be on path
-if [[ -n "${ZOWE_JAVA_HOME}" ]]
-then 
-    ls ${ZOWE_JAVA_HOME}/bin | grep java$ > /dev/null    # pick a file to check
-    if [[ $? -ne 0 ]]
-    then
-        echo Error: ZOWE_JAVA_HOME does not point to a valid install of Java
-    else
-      JAVA_VERSION=`${ZOWE_JAVA_HOME}/bin/java -version 2>&1 | grep ^"java version"`
-      if [[ "$JAVA_VERSION" < "java version \"1.8" ]]
-      then 
-        echo Error: $JAVA_VERSION is less than minimum level required of 1.8
-      fi
-    fi
-else 
-    echo Error: ZOWE_JAVA_HOME is empty
-fi
+. ${ROOT_DIR}/scripts/utils/validateJava.sh
 
-return $FILES_API_ERRORS_FOUND
+return $ERRORS_FOUND-$INITIAL_ERRORS_FOUND
