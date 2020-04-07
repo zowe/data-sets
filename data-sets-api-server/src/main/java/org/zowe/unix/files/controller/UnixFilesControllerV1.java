3f89cd69828d5556e6be6c39e0807c35f2e0f599
Nakul Manchanda <nakul.manchanda@ibm.com>
2020-04-07 17:52:32 -0400

    add auth header to integration tests
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- data-sets-api-server/src/main/java/org/zowe/unix/files/controller/UnixFilesControllerV1.java
+++ data-sets-api-server/src/main/java/org/zowe/unix/files/controller/UnixFilesControllerV1.java
@@ -1,11 +1,21 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2020
+ */
+
 package org.zowe.unix.files.controller;
+
+import io.swagger.annotations.Api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zowe.unix.files.services.UnixFilesService;
-
-import io.swagger.annotations.Api;
 
 @RestController
 @RequestMapping("/api/v1/unixfiles")
---
+++ data-sets-tests/src/test/java/org/zowe/unix/files/tests/UnixFilesLogoutIntegrationTest.java
@@ -0,0 +1,39 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2019
+ */
+package org.zowe.unix.files.tests;
+
+import io.restassured.RestAssured;
+
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
+public class UnixFilesLogoutIntegrationTest extends AbstractUnixFilesIntegrationTest {
+    final String path = "?path=" + TEST_DIRECTORY;
+
+    @Test
+    public void testGetDirectoryListingWithoutAuth() throws Exception {
+        RestAssured.given().header(AUTH_HEADER).when().auth().none().get(path)
+            .then().statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
+
+    @Test
+    public void testLogoutWithGetDirectoryListing() throws Exception {
+        String cookie = RestAssured.given().header(AUTH_HEADER).when().get(path).getCookie("JSESSIONID");
+
+        RestAssured.given().header(AUTH_HEADER).when().auth().none().cookie("JSESSIONID", cookie).get(path)
+            .then().statusCode(HttpStatus.SC_OK);
+
+        RestAssured.given().header(AUTH_HEADER).when().auth().none().cookie("JSESSIONID", cookie).get("/logout")
+            .then().statusCode(HttpStatus.SC_NO_CONTENT);
+
+        RestAssured.given().header(AUTH_HEADER).when().auth().none().cookie("JSESSIONID", cookie).get(path)
+        .then().statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
+}
