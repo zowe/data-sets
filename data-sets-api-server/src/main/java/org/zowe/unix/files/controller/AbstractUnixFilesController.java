d0d80dde8c4f90263fb5f9c9d5afd7ddd6b82ba6
Jordan Cain <jordan.cain1@uk.ibm.com>
2020-04-15 10:20:38 -0400

    Remove tag creating duplicate swagger entries
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

--- data-sets-api-server/src/main/java/org/zowe/unix/files/controller/AbstractUnixFilesController.java
+++ data-sets-api-server/src/main/java/org/zowe/unix/files/controller/AbstractUnixFilesController.java
@@ -55,7 +55,7 @@ public abstract class AbstractUnixFilesController {
     }
     
     @GetMapping(value = "", produces = { "application/json" })
-    @ApiOperation(value = "Get a list of a directories contents", nickname = "getDirectoryListing", notes = "This API gets a list of files and directories for a given path", tags = "Unix Files APIs")
+    @ApiOperation(value = "Get a list of a directories contents", nickname = "getDirectoryListing", notes = "This API gets a list of files and directories for a given path")
     @ApiResponses({ @ApiResponse(code = 200, message = "Ok", response = UnixDirectoryAttributesWithChildren.class) })
     public UnixDirectoryAttributesWithChildren getUnixDirectoryListing(
             @ApiParam(value = "Path of Directory to be listed", required = true) @RequestParam String path, HttpServletRequest request) {
