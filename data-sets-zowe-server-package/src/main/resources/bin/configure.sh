e65c14725bbda9fe18e72121e9d250908ac20a65
stevenhorsman <steven@uk.ibm.com>
2020-11-06 16:25:28 +0000

    unignore bin scripts
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

---
+++ data-sets-zowe-server-package/src/main/resources/bin/configure.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+################################################################################
+# This program and the accompanying materials are made available under the terms of the
+# Eclipse Public License v2.0 which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-v20.html
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Copyright IBM Corporation 2019, 2020
+################################################################################
+
+# Add static definition for files-api
+cat <<EOF >${STATIC_DEF_CONFIG_DIR}/files-api.ebcidic.yml
+#
+services:
+  - serviceId: datasets
+    title: IBM z/OS Datasets
+    description: IBM z/OS Datasets REST API service
+    catalogUiTileId: datasetsAndUnixFiles
+    instanceBaseUrls:
+      - https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/
+    homePageRelativeUrl:  # Home page is at the same URL
+    routedServices:
+      - gatewayUrl: api/v1  # [api/ui/ws]/v{majorVersion}
+        serviceRelativeUrl: api/v1/datasets
+      - gatewayUrl: api/v2  # [api/ui/ws]/v{majorVersion}
+        serviceRelativeUrl: api/v2/datasets
+    apiInfo:
+      - apiId: org.zowe.data.sets
+        gatewayUrl: api/v1
+        version: 1.0.0
+        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/v2/api-docs
+        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/swagger-ui.html
+      - apiId: org.zowe.data.sets
+        gatewayUrl: api/v2
+        version: 2.0.0
+        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/v2/api-docs
+        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/swagger-ui.html
+    customMetadata:
+      apiml:
+        enableUrlEncodedCharacters: true
+  - serviceId: unixfiles
+    title: IBM z/OS Unix Files
+    description: IBM z/OS Unix Files REST API service
+    catalogUiTileId: datasetsAndUnixFiles
+    instanceBaseUrls:
+      - https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/
+    homePageRelativeUrl:  # Home page is at the same URL
+    routedServices:
+      - gatewayUrl: api/v1  # [api/ui/ws]/v{majorVersion}
+        serviceRelativeUrl: api/v1/unixfiles 
+      - gatewayUrl: api/v2  # [api/ui/ws]/v{majorVersion}
+        serviceRelativeUrl: api/v2/unixfiles 
+    apiInfo:
+      - apiId: org.zowe.unix.files
+        gatewayUrl: api/v1
+        version: 1.0.0
+        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/v2/api-docs
+        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/swagger-ui.html
+      - apiId: org.zowe.unix.files
+        gatewayUrl: api/v2
+        version: 2.0.0
+        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/v2/api-docs
+        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${FILES_API_PORT}/swagger-ui.html
+catalogUiTiles:
+  datasetsAndUnixFiles:
+    title: z/OS Datasets and Unix Files services
+    description: IBM z/OS Datasets and Unix Files REST services
+EOF
+iconv -f IBM-1047 -t IBM-850 ${STATIC_DEF_CONFIG_DIR}/files-api.ebcidic.yml > $STATIC_DEF_CONFIG_DIR/files-api.yml
+rm ${STATIC_DEF_CONFIG_DIR}/files-api.ebcidic.yml
+chmod 770 $STATIC_DEF_CONFIG_DIR/files-api.yml
---
+++ data-sets-zowe-server-package/src/main/resources/bin/start.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+################################################################################
+# This program and the accompanying materials are made available under the terms of the
+# Eclipse Public License v2.0 which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-v20.html
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Copyright IBM Corporation 2018, 2020
+################################################################################
+
+# Variables required on shell:
+# - ZOWE_PREFIX
+# - FILES_API_PORT - The port the data sets server will use
+# - KEY_ALIAS
+# - KEYSTORE - The keystore to use for SSL certificates
+# - KEYSTORE_TYPE - The keystore type to use for SSL certificates
+# - KEYSTORE_PASSWORD - The password to access the keystore supplied by KEYSTORE
+# - KEY_ALIAS - The alias of the key within the keystore
+# - GATEWAY_PORT - The SSL port z/OSMF is listening on.
+# - ZOWE_EXPLORER_HOST - The IP Address z/OSMF can be reached
+
+COMPONENT_ROOT_DIR=$(cd $(dirname $0)/..;pwd)
+# the jar file is placed in the root folder of the component package
+JAR_FILE=$(ls -1 data-sets-api-server-*.jar)
+
+stop_jobs()
+{
+  kill -15 $pid
+}
+
+trap 'stop_jobs' INT
+
+COMPONENT_CODE=EF
+_BPX_JOBNAME=${ZOWE_PREFIX}${COMPONENT_CODE} java \
+  -Xms16m -Xmx512m \
+  -Dibm.serversocket.recover=true \
+  -Dfile.encoding=UTF-8 \
+  -Djava.io.tmpdir=/tmp -Xquickstart \
+  -Dserver.port=${FILES_API_PORT} \
+  -Dserver.ssl.keyAlias=${KEY_ALIAS} \
+  -Dserver.ssl.keyStore=${KEYSTORE} \
+  -Dserver.ssl.keyStorePassword=${KEYSTORE_PASSWORD} \
+  -Dserver.ssl.keyStoreType=${KEYSTORE_TYPE} \
+  -Dserver.connection-timeout=8000 \
+  -Dcom.ibm.jsse2.overrideDefaultTLS=true \
+  -Dserver.compression.enabled=true \
+  -Dconnection.httpsPort=${GATEWAY_PORT} \
+  -Dconnection.ipAddress=${ZOWE_EXPLORER_HOST} \
+  -Dspring.main.banner-mode=off \
+  -Djava.protocol.handler.pkgs=com.ibm.crypto.provider \
+  -jar "$COMPONENT_ROOT_DIR/$JAR_FILE" &
+pid=$?
+
+wait
