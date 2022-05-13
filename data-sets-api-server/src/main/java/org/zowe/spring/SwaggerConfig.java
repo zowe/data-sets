d9c49bae1503bdf64ce621e46398672414b2092e
Carson Cook <carson.cook@ibm.com>
2022-05-13 14:14:38 -0400

    Split up swagger docs for API Catalog
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- data-sets-api-server/src/main/java/org/zowe/spring/SwaggerConfig.java
+++ data-sets-api-server/src/main/java/org/zowe/spring/SwaggerConfig.java
@@ -10,12 +10,9 @@
 package org.zowe.spring;
 
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Collections;
-
+import org.springframework.context.annotation.Primary;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -23,18 +20,74 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
+    @Primary
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("all")
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.regex("/api.*"))
                 .build()
                 .apiInfo(
-                    new ApiInfo("Files API", "REST API for the Data sets and z/OS Unix Files Services", "1.0", null, null, null, null, Collections.emptyList())
+                        new ApiInfo("Files API", "REST API for the Data sets and z/OS Unix Files Services", "1.0", null, null, null, null, Collections.emptyList())
+                );
+    }
+
+    @Bean
+    public Docket apiV1Datasets() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("apiV1Datasets")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/api/v1/datasets.*"))
+                .build()
+                .apiInfo(
+                        new ApiInfo("Datasets API", "REST API for the Data sets Service", "1.0", null, null, null, null, Collections.emptyList())
+                );
+    }
+
+    @Bean
+    public Docket apiV2Datasets() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("apiV2Datasets")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/api/v2/datasets.*"))
+                .build()
+                .apiInfo(
+                        new ApiInfo("Datasets API", "REST API for the Data sets Service", "2.0", null, null, null, null, Collections.emptyList())
+                );
+    }
+
+    @Bean
+    public Docket apiV1UnixFiles() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("apiV1UnixFiles")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/api/v1/unixfiles.*"))
+                .build()
+                .apiInfo(
+                        new ApiInfo("Unix Files API", "REST API for the z/OS Unix Files Service", "1.0", null, null, null, null, Collections.emptyList())
+                );
+    }
+
+    @Bean
+    public Docket apiV2UnixFiles() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("apiV2UnixFiles")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/api/v2/unixfiles/.*"))
+                .build()
+                .apiInfo(
+                        new ApiInfo("Unix Files API", "REST API for the z/OS Unix Files Service", "2.0", null, null, null, null, Collections.emptyList())
                 );
     }
 }
