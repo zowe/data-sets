3c67e3010569c0fed3396cd1e6e384b0a60177b6
Carson Cook <carson.cook@ibm.com>
2022-05-16 09:14:50 -0400

    Fix imports
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- data-sets-api-server/src/main/java/org/zowe/spring/SwaggerConfig.java
+++ data-sets-api-server/src/main/java/org/zowe/spring/SwaggerConfig.java
@@ -12,7 +12,9 @@ package org.zowe.spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+
+import java.util.Collections;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -20,13 +22,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    @Primary
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("all")
